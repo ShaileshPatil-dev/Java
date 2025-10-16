31f58d15d5ef837559454c4fc03038b3b8b04f80
Shailesh Patil <scpatil2004@gmail.com>
2025-10-16 17:58:14 +0530

    fix error

--- Print.java
+++ Print.java
@@ -1,6 +1,6 @@
 public class Print {
     public static void main(String[] args) {
-        System.out.println("This will print the statement on same line");
+        System.out.print("This will print the statement on same line");
         System.out.println("This will print the statement on new line");
     }
 }
