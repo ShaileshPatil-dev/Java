ac9b111249691844465be049fcd3cd402a3ef2b2
Shailesh Patil <scpatil2004@gmail.com>
2026-07-21 23:32:40 +0530

    insertion and selection sort

---
+++ src/video17/Selection.java
@@ -0,0 +1,33 @@
+package video17;
+
+import java.util.Arrays;
+
+public class Selection {
+
+     public static void main(String[] args) {
+          int[] unsortedarray = { 19, 73, 64, 28, 55 };
+          SelectionSort(unsortedarray);
+          System.out.println(Arrays.toString(unsortedarray));
+     }
+
+     static void SelectionSort(int[] array) {
+          for (int i = 0; i < array.length; i++) {
+               int lastPosition = array.length - i - 1;
+               int max = FindMax(array, 0, lastPosition);
+               int temp = array[lastPosition];
+               array[lastPosition] = array[max];
+               array[max] = temp;
+          }
+     }
+
+     static int FindMax(int[] array, int start, int end) {
+          int max = start;
+          for (int i = start; i <= end; i++) {
+               if (array[i] > array[max]) {
+                    max = i;
+               }
+          }
+
+          return max;
+     }
+}
---
+++ src/video17/insertion.java
@@ -0,0 +1,25 @@
+package video17;
+
+import java.util.Arrays;
+
+public class insertion {
+public static void main(String[] args) {
+     int[] array = {1,9,2,6,3,7,4,8};
+     InsertionSort(array);
+     System.out.println(Arrays.toString(array));
+}
+static void InsertionSort(int[] Array){
+     for (int i = 0; i < Array.length-1; i++) {
+          for (int j = i+1; j >0; j--) {
+               if (Array[j]<Array[j-1]) {
+                    int temp = Array[j-1];
+                    Array[j-1]= Array[j];
+                    Array[j]=temp;
+               }
+               else{
+                    break;
+               }
+          }
+     }
+}
+}
