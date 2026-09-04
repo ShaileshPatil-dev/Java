d153c5e4022411da6319ae22d4a468369e4e7282
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-09-04 14:48:51 +0530

    hashmap

--- src/video21/LearnHashmap.java
+++ src/video21/LearnHashmap.java
@@ -2,18 +2,18 @@ package video21;
 // import java.util.HashMap;
 public class LearnHashmap {
  public static void main(String[] args) {
-     // String s = "banana"; 
+     String s = "banana"; 
      
-     // HashMap<Character , Integer> map = new HashMap<>() ; 
-     // for (int i = 0 ; i < s.length() ; i++)
-     //      {char ch = s.charAt(i); 
-     //           if(map.containsKey(ch)){
-     //                map.put(ch,map.get(ch)+1);
-     //           }
-     //           else{
-     //                map.put(ch,1);
-     //           }
-     //      }
+     HashMap<Character , Integer> map = new HashMap<>() ; 
+     for (int i = 0 ; i < s.length() ; i++)
+          {char ch = s.charAt(i); 
+               if(map.containsKey(ch)){
+                    map.put(ch,map.get(ch)+1);
+               }
+               else{
+                    map.put(ch,1);
+               }
+          }
      int a = (int) 'a' ;
      System.out.println(a);
  }
